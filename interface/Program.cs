6131d3070d0d271f2d943e087d95f0a8fdbd7504
GT <v244682@tafe.wa.edu.au>
2019-05-28 13:58:21 +0800

    first commit

---
+++ interface/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace InterfaceEmployee
+{
+    class Program
+    {
+       static public List<employeeid> em = new List<employeeid>();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Employee App");
+            Console.WriteLine("Id, role, Name");
+           em.Add(new employee("Sue", "Manager", 3));
+            displayData();
+            createEmployee();
+
+
+            Console.ReadLine();
+
+        }
+
+        static public void createEmployee()
+        {
+            Console.WriteLine("Would you like to add a new employee Y or N");
+            if (Console.ReadLine() == "Y")
+            {
+                Console.WriteLine("Add new employee, press enter Name");
+                string name = Console.ReadLine();
+                Console.WriteLine("Now enter Role");
+                string role = Console.ReadLine();
+                Console.WriteLine("Now enter Id");
+                int ID = Convert.ToInt32(Console.ReadLine());
+                em.Add(new employee(name, role, ID));
+                Console.Clear();
+                displayData();
+            }
+            else
+            {
+                Console.WriteLine("Would you like to close the App? Y or N");
+                if (Console.ReadLine() == "Y")
+                {
+                   
+                    Console.Clear();
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    createEmployee();
+
+                }
+            }
+        }
+
+
+        static public void displayData()
+        {
+            for (int i = 0; i < em.Count; i++)
+            {
+                Console.WriteLine(em[i].ID + " " + em[i].Role + " " + em[i].Name);
+            }
+            createEmployee();
+        }
+
+    }
+}
---
+++ interface/employee.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterfaceEmployee
+{
+  public  interface employeeid
+    {
+        string Name
+        {
+            get;
+            set;
+        }
+        string Role
+        {
+            get;
+            set;
+        }
+
+        int ID
+        {
+            get;
+            set;
+        }
+
+    }
+
+    class employee : employeeid
+    {
+        private string name;
+        private string role;
+        private int id;
+
+        /// <summary>
+        /// constructor
+        /// </summary>
+        /// <param name="Name"></param>
+        /// <param name="Role"></param>
+        /// <param name="ID"></param>
+        public employee(string Name, string Role, int ID )
+        {
+            name = Name;
+            role = Role;
+            id = ID;
+        }
+
+        //Setter & getter
+        public string Name  
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public string Role
+        {
+            get
+            {
+                return role;
+            }
+            set
+            {
+                role = value;
+            }
+        }
+
+        public int ID
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                id = value;
+            }
+        }
+
+    }
+}
